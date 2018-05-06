dc7f55cf55ffe48544cafde1006d351d96e58659
ramanans <s.venkatramanan@gmail.com>
2018-05-06 13:02:38 +0530

    Update SqRoot.java

--- SqRoot.java
+++ SqRoot.java
@@ -1,9 +1,9 @@
 public class SqRoot {
-    public static void main(String args[]) {
-        System.out.println("sq root of given no = " + sqrt(Integer.parseInt(args[0])));
+public static void main(String args[]) {
+        System.out.println("sq root of given no = " + sqrt(Double.parseDouble(args[0])));
     }
 
- public static double sqrt(int number) {
+    public static double sqrt(double number) {
 	double t;
  
 	double squareRoot = number / 2;
